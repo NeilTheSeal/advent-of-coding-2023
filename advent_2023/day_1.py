54e34f5092ca6068cacd87b4c5c45964ca1c53ba
NeilTheSeal <neil.hendren@gmail.com>
2024-10-04 22:10:34 -0600

    Initial commit

---
+++ advent_2023/day_1.py
@@ -0,0 +1,9 @@
+# These methods will be exported as a module to the main script
+
+
+def part_1():
+    return 0
+
+
+def part_2():
+    return 0
---
+++ tests/test_day_1.py
@@ -0,0 +1,9 @@
+from advent_2023.day_1 import part_1, part_2
+
+
+class TestDay1:
+    def test_part_1(self):
+        assert part_1() == 0
+
+    def test_part_2(self):
+        assert part_2() == 0
